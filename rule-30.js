88f943f2b0e8b2326190db141efa558703e8af64
Eli Tan <eliedrian1996@gmail.com>
2016-04-16 22:33:17 +0800

    refactor and more progress on implementation

--- rule-30.js
+++ rule-30.js
@@ -1,22 +1,42 @@
 var seed = document.getElementById("seed");
 var width = seed.offsetWidth;
 // number of buttons to create for detected width
-var squaresToCreate = Math.floor(width / 20.6);
+var squaresToCreate = Math.floor(width / 20.6) - 1;
 var squareTemplate = document.getElementById("square");
 
-function generateFirstRow(numberOfSquares) {
+function appendRow(containerId, numberOfSquares, rowCount) {
+	var rowCount = rowCount || 1;
 	var count = 1;
 	var button = squareTemplate.content;
+	var appended = [];
+	var container = document.createElement("div");
+	container.id = containerId + "-row" + rowCount;
 	while (count < numberOfSquares) {
 		clone = document.importNode(button, true);
 		var buttonElement = clone.querySelector("button");
-		buttonElement.id = count++;
+		buttonElement.id = containerId + count++;
 		buttonElement.addEventListener("click", function() {
-			this.classList.toggle("off")
-			this.classList.toggle("on")
+			this.classList.toggle("off");
+			this.classList.toggle("on");
 		});
-		seed.appendChild(clone);
+		appended.push(clone.firstElementChild);
+		container.appendChild(clone);
 	}
+	document.getElementById(containerId).appendChild(container);
+	return appended;
 }
-generateFirstRow(squaresToCreate);
 
+appendRow("seed", squaresToCreate);
+
+document.addEventListener("keypress", function(e) {
+	e.preventDefault();
+	if (e.keyCode != 13) {
+		return false;
+	}
+	var row = appendRow("result", squaresToCreate);
+	for (var i = 0; i < row.length; i++) {
+		var obj = row[i];
+
+	}
+	return false;
+});
